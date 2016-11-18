59ba8045c8d7c25e35192ab1e4cedea7c8a11a5b
Kelly Ochikubo <Kelly@Kellys-MacBook-Pro.local>
2016-11-17 21:48:00 -0800

    docstrings cuz

--- graph/format_data.py
+++ graph/format_data.py
@@ -6,18 +6,43 @@ from sklearn.preprocessing import LabelEncoder
 
 
 def connect_to_database(database_name):
+    '''
+    Connect to MongoDB (must have Mongo server running to connect)
+
+    INPUT:
+        database_name (st)
+    OUTPUT:
+        db (MongoDB)
+    '''
     client = MongoClient()
     db = client[database_name]
     return db
 
 
 def convert_collection_to_df(db, collection_name):
+    '''
+    Convert collection in MongoDB to pandas dataframe
+
+    INPUT:
+        db (MongoDB): mongo database instance
+        collection_name (str)
+    OUTPUT:
+        df (DataFrame)
+    '''
     input_data = db[collection_name]
     df = pd.DataFrame(list(input_data.find()))
     return df
 
 
 def clean_data(df):
+    '''
+    Clean and reformat dataframe
+
+    INPUT:
+        df (DataFrame): unclean dataframe
+    OUTPUT:
+        df (DataFrame): clean dataframe
+    '''
     df.drop('_id', axis=1, inplace=True)
     df.drop_duplicates(['state', 'topic', 'user', 'text'])
     df.topic = df.topic.apply(lambda top: top.replace(" (Closed topic)", ""))
@@ -29,30 +54,72 @@ def clean_data(df):
 
 
 def get_post_text(df):
+    """
+    Retrieve all the posts' text for respective topics
+
+    INPUT:
+        df (DataFrame): cleaned dataframe
+    OUTPUT:
+        df (DataFrame)
+    """
     return df[['topic_id', 'text']].drop_duplicates()
 
 
 def get_users(df):
+    """
+    Retrieve all user id's and respective usernames
+
+    INPUT:
+        df (DataFrame): cleaned dataframe
+    OUTPUT:
+        df (DataFrame)
+    """
     return df[['user_id', 'user']].drop_duplicates()
 
 
 def get_topics(df):
+    """
+    Retrieve all topic id's and respective topics
+
+    INPUT:
+        df (DataFrame): cleaned dataframe
+    OUTPUT:
+        df (DataFrame)
+    """
     return df[['topic_id', 'topic']].drop_duplicates()
 
 
-def format_adjacency_list(clean):
-    df = clean.groupby(['topic_id']).user_id.apply(lambda x: " ".join(str(x) for x in x.values))
+def format_adjacency_list(df):
+    """
+    Create an adjacency list from a dataframe and write to csv file
+
+    INPUT:
+        df (DataFrame): cleaned dataframe
+    """
+    df = df.groupby(['topic_id']).user_id.apply(lambda x: " ".join(str(x) for x in x.values))
     df.to_csv("adj_list.csv", index=False, header=False)
 
 
-def format_edges_combinations(clean):
-    topic_user = clean[['topic_id', 'user_id']]
+def format_edges_combinations(df):
+    """
+    Create edge list from dataframe using combinations and write to tsv file
+
+    INPUT:
+        df (DataFrame): cleaned dataframe
+    """
+    topic_user = df[['topic_id', 'user_id']]
     combos = topic_user.groupby('topic_id').user_id.apply(lambda x: list(combinations(x, 2)))
     edges = pd.DataFrame(reduce(lambda x, y: x + y, combos))
     edges.to_csv('users_as_edges_combo.tsv', sep='\t', header=False, index=False, encoding='utf-8')
 
 
 def format_edges(clean):
+    """
+    Create edge list from dataframe and write to tsv file
+
+    INPUT:
+        df (DataFrame): cleaned dataframe
+    """
     df = pd.DataFrame(clean.groupby(['topic_id', 'user_id']).count())['text'].reset_index()
     df.rename(columns={'text': 'count'}, inplace=True)  # use for weighted edges
     t1 = df[['user_id', 'topic_id']]
@@ -66,6 +133,15 @@ def format_edges(clean):
 
 
 def combining_dataframes(df1, df2):
+    """
+    Combine two dataframes
+
+    INPUT:
+        df1 (DataFrame)
+        df2 (DataFrame)
+    OUTPUT:
+        new_df (DataFrame)
+    """
     new_df = pd.concat([df1, df2])
     return new_df
 
