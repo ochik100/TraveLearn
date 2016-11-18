e28101d579ff64900e3d0161e333b2cb53a68bf5
Kelly Ochikubo <Kelly@Kellys-MacBook-Pro.local>
2016-11-17 17:36:05 -0800

    setup env update

--- setup_ec2_environment.sh
+++ setup_ec2_environment.sh
@@ -7,5 +7,8 @@ sudo pip install beautifulsoup4
 sudo pip install requests
 sudo apt --assume-yes install mongodb-clients
 sudo apt --assume-yes  install mongodb-server
+# pip install --user numpy scipy matplotlib ipython pandas
+# pip install -U scikit-learn
+# sudo pip install networkx
 sudo pip install s3cmd
 s3cmd --configure
