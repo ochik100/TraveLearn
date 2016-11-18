016b94dca0973f9d53859692277af32b463879e4
Kelly Ochikubo <Kelly@Kellys-MacBook-Pro.local>
2016-11-17 21:59:27 -0800

    more docstrings

--- graph/network_graph.py
+++ graph/network_graph.py
@@ -7,40 +7,86 @@ import pandas as pd
 
 
 def load_data(filename):
+    """
+    Load data into a pandas dataframe from a file
+
+    INPUT:
+        filename (str)
+    OUTPUT:
+        df (DataFrame)
+    """
     df = pd.read_json(filename)
     return df
 
 
 def create_edges_file(df, filename):
+    """
+    Create edges file from dataframe (this format is used for Gephi)
+
+    INPUT:
+        df (DataFrame)
+        filename (str)
+    """
     ndf = df.groupby(['topic', 'user']).date_time.count().reset_index()
     ndf.drop('date_time', axis=1, inplace=True)
     ndf.columns = ['source', 'target']
     ndf.to_csv(filename, sep='\t', header=True, encoding='utf-8', index=False)
-    return None
 
 
 def create_graph_from_edges_file(filename):
-    TG = nx.read_edgelist(filename, delimiter='\t')
-    return TG
+    """
+    Create a graph from an edge list given the filename
+
+    INPUT:
+        filename (str)
+    OUTPUT:
+        G (graph)
+    """
+    G = nx.read_edgelist(filename, delimiter='\t')
+    return G
 
 
 def create_graph_from_adjaceny_list_file(filename):
+    """
+    Create a graph from an adjacency list given the filename
+
+    INPUT:
+        filename (str)
+    OUTPUT:
+        AG (graph)
+    """
     AG = nx.read_adjlist(filename)
     return AG
 
 
 def get_largest_connected_component_of_graph(G):
+    '''
+    Get the largest connected component of a graph (essentially, remove all components not connected to the largest connected component of a graph)
+
+    INPUT:
+        G (graph): graph of entire network
+    OUTPUT:
+        LG (graph): graph of largest connected component
+    '''
     return list(nx.connected_component_subgraphs(AG))[0]
 
 
 def find_communities(G):
+    """
+    Find communities within a graph, and plot the graph with the communities shown in different colors
+
+    INPUT:
+        G (graph)
+    OUTPUT:
+        parts (dict): a dictionary where each key is a node in the graph and its value is its associated community (a number from 0 to the number of communities found within the graph)
+    """
     spring_pos = nx.spring_layout(G)
     parts = community.best_partition(G)
     values = [parts.get(node) for node in G.nodes()]
     plt.axis("off")
     nx.draw_networkx(G, pos=spring_pos, cmap=plt.get_cmap("jet"),
                      node_color=values, node_size=20, with_labels=False)
-    # plt.show()
+    plt.show()
     return parts
 
 
