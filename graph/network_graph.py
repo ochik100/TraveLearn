9f3100f844f8528a301266df4718e3a50c3cad49
Kelly Ochikubo <Kelly@Kellys-MacBook-Pro.local>
2016-11-21 13:32:48 -0800

    networkxgraph docstrings

--- graph/network_graph.py
+++ graph/network_graph.py
@@ -36,6 +36,12 @@ class NetworkXGraph(object):
         return self.df[['topic_id', 'topic']].drop_duplicates().set_index('topic_id')
 
     def create_edges_dataframe(self):
+        '''
+        Create an edges dataframe from the cleaned data
+
+        OUTPUT:
+            edges (dataframe): dataframe with topic ids and combinations of user ids for each topic
+        '''
         edges = self.df.groupby('topic_id').user_id.apply(
             lambda user_id: self.helper_get_edges_combination(user_id.values))
         print "Created edge list from dataframe"
@@ -47,10 +53,22 @@ class NetworkXGraph(object):
         return zip(np.repeat(ids[0], l), ids[1:])
 
     def helper_get_edges_combination(self, user_id):
+        '''
+        Helper function to create a list of all the user interactions present in one topic
+
+        OUTPUT:
+            A list of tuples
+        '''
         ids = np.unique(user_id)
         return [x for x in combinations(ids, 2)]
 
     def create_graph_from_edges(self):
+        '''
+        Create a NetworkX graph from an edges dataframe, where nodes are user ids, edges are formed if two users have interacted on a forum, and edges have the topic id of the forum interacted on
+
+        OUTPUT:
+            G (NetworkX Graph)
+        '''
         G = Graph()
         for topic_id, user_ids in self.edges.iteritems():
             G.add_edges_from(user_ids, topic_id=topic_id)
@@ -59,6 +77,12 @@ class NetworkXGraph(object):
         return G
 
     def create_randomly_sampled_subgraph_of_graph(self):
+        '''
+        Randomly sample 1/10 of the number of nodes from the original graph using the reservoir sampling algorithm.
+
+        OUTPUT:
+            subgraph_nodes (list): a list of the randomly sampled nodes
+        '''
         num_nodes = self.graph.number_of_nodes() * (1. / 10)
         subgraph_nodes = []
         for i, node in enumerate(self.graph.nodes_iter()):
