19f7c3e0b52d7e430e5dfdc38d1f54b708da140e
Kelly Ochikubo <Kelly@Kellys-MacBook-Pro.local>
2016-11-29 14:34:35 -0800

    modified setup

--- setup.sh
+++ setup.sh
@@ -9,12 +9,7 @@ sudo apt --assume-yes install mongodb-clients
 sudo apt --assume-yes  install mongodb-server
 sudo apt-get install python-tk
 sudo pip install pandas scikit-learn matplotlib
-sudo apt-get install python-scipy
-sudo pip install networkx python-louvain scipy numpy
-# pip install --user numpy scipy matplotlib ipython pandas
-# pip install -U scikit-learn
-# sudo pip install networkx
-# sudo apt-get install libxml2-dev libgmp-dev zlib1g-dev libigraph0-dev
-# sudo pip install python-igraph
-sudo pip install s3cmd
-s3cmd --configure
+sudo apt-get install python-scipy scipy numpy
+sudo pip install networkx python-louvain
+# sudo pip install s3cmd
+# s3cmd --configure
